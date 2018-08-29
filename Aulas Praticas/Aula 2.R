6fe9c87f63e06539e39f5e8ae9ef88c928de1a36
Daniel-EST <user@exemplo.com>
2018-08-28 21:50:25 -0300

    Adicionei a extensão *.Rproj ao gitignore. Coloquei nosso programa que gera variáveis aleatória de uma Poisson de parâmentro lambda.

---
+++ Aulas Praticas/Aula 2.R
@@ -0,0 +1,11 @@
+r_poisson <- function(lambda){
+  u <- runif(1)
+  i <- 0 ; p <- exp(-lambda) ; f <- p
+  while(u >= f){
+    i <- i + 1
+    p <- p * lambda/i
+    f <- f + p 
+  }
+  x <- i
+  return(x)
+}
