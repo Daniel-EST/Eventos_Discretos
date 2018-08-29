94b33cc6761469ca0f05360ee7d40958f4400157
Gabriel Mizuno <40043614+GMizuno@users.noreply.github.com>
2018-08-29 00:10:00 -0300

    Concertei o arquivo anterior

--- Aulas Praticas/Aula 2.1.R
+++ Aulas Praticas/Aula 2.1.R
@@ -1,10 +1,16 @@
-poisson=function(lambda){
-  u <- runif(1);i <- 0;p <- exp(-1*lambda);f <-p
-  while(u>=f){
-    i <- i+1;p <- p*lambda/i;f <- f+p
-    print(f)
+poisson=function(lambda,n){
+  x <- 0
+  y <- 1:n
+  u <- runif(n)
+  i <- 0;p <- exp(-1*lambda);f <-p
+  for (j in y) {
+    while(u[j]>=f){
+      i <- i+1;p <- p*lambda/i;f <- f+p
+    }
+    x[j] <- i
+    i <- 0;p <- exp(-1*lambda);f <-p
   }
-  x <- i
-  return(x)
+  print(x)
+  mean(x)
 }
-poisson(500)
+poisson(10,400)
