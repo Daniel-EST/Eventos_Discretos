2763c406e2ce1f180e30ae0d4967f8657675f0f5
Gabriel Mizuno <40043614+GMizuno@users.noreply.github.com>
2018-09-01 15:44:58 -0300

    Acrescentei uma questao e modifiquei o arquivo 2.2

--- Aulas Praticas/Aula 2.2(Contando o numero de passos).R
+++ Aulas Praticas/Aula 2.2(Contando o numero de passos).R
@@ -1,17 +1,14 @@
 poisson=function(lambda,n){
   x <- 0
-  u <- runif(n)
-  passos <- 1:n
-  for (j in 1:n){
-    i <- 0;p <- exp(-1*lambda);f <-p;cont <- 1
-    while(u[j]>=f){
-      i <- i+1;p <- p*lambda/i;f <- f+p
-      cont<- cont+1
-    }
-    x[j] <- i
-    passos[j] <- cont
+  u <- runif(1)
+  i <- 0;p <- exp(-1*lambda);f <-p;cont <- 1
+  while(u>=f){
+    i <- i+1;p <- p*lambda/i;f <- f+p
+    cont<- cont+1
   }
-  cat('Media do nuumero de passos para gerar X;',mean(passos))
+  x <- i
+  passos <- cont
+  cat('Media do numero de passos para gerar X;',passos)
 }
-poisson(10,400)
+poisson(10)
 
