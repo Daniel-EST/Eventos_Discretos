4ddf62706e8ce65a1b054b560e56c30f0ea3febb
Gabriel Mizuno <40043614+GMizuno@users.noreply.github.com>
2018-09-01 16:08:43 -0300

    Tirei algumas coisas

--- Aulas Praticas/Aula 2.1.R
+++ Aulas Praticas/Aula 2.1.R
@@ -1,16 +1,12 @@
 poisson=function(lambda,n){
   x <- 0
-  y <- 1:n
-  u <- runif(n)
+  u <- runif(1)
   i <- 0;p <- exp(-1*lambda);f <-p
-  for (j in y) {
-    while(u[j]>=f){
+  while(u>=f){
       i <- i+1;p <- p*lambda/i;f <- f+p
     }
-    x[j] <- i
+    x <- i
     i <- 0;p <- exp(-1*lambda);f <-p
-  }
-  print(x)
-  mean(x)
+  cat("Némero gerado é;",x)
 }
-poisson(10,400)
+poisson(10)
