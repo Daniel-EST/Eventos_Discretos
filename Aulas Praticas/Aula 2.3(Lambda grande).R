bcd2ccf1328a17fef3cb818aa1da6bf75339dfaf
Gabriel Mizuno <40043614+GMizuno@users.noreply.github.com>
2018-09-01 14:18:49 -0300

    poisson para lambda grande

---
+++ Aulas Praticas/Aula 2.3(Lambda grande).R
@@ -0,0 +1,20 @@
+poisson <- function(lambda){
+  u <- runif(1);I <- as.integer(lambda); f <- ppois(I,lambda); passos <- 1
+  if (u<f){
+    i <- I; p <- dpois(i,lambda); f <- f-p
+    while (u<f){
+      i <- i-1; p <- dpois(i,lambda); f <- f-p
+    }
+    x <- i
+    return(x)
+  }
+  else{
+    i <- I+1; p <- dpois(i,lambda); f <- f+p
+    while (u>=f){
+      i <- i+1; p <- p*lambda/i; f <- f+p
+    }
+    x <- i+1
+    return(x)
+  }
+}
+poisson(1000)
