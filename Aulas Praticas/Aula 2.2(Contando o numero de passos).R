4735f8907b0cee8f7d321acf03d66e47e2914225
Gabriel Mizuno <40043614+GMizuno@users.noreply.github.com>
2018-09-01 16:04:44 -0300

    Tirei coisas inuties

--- Aulas Praticas/Aula 2.2(Contando o numero de passos).R
+++ Aulas Praticas/Aula 2.2(Contando o numero de passos).R
@@ -1,12 +1,10 @@
 poisson=function(lambda,n){
-  x <- 0
   u <- runif(1)
   i <- 0;p <- exp(-1*lambda);f <-p;cont <- 1
   while(u>=f){
     i <- i+1;p <- p*lambda/i;f <- f+p
     cont<- cont+1
   }
-  x <- i
   passos <- cont
   cat('Media do numero de passos para gerar X;',passos)
 }
