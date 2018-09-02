1a0431900545cbcbe817a18bce0d8912cdef2066
Gabriel Mizuno <40043614+GMizuno@users.noreply.github.com>
2018-09-02 11:50:25 -0300

    Numero de interacoes para lambda grande

--- Aulas Praticas/Aula 2.4(Lambda grande numero de passos).R
+++ Aulas Praticas/Aula 2.4(Lambda grande numero de passos).R
@@ -1,20 +1,24 @@
 poisson <- function(lambda){
-  u <- runif(1);I <- as.integer(lambda); f <- ppois(I,lambda)
+  u <- runif(1);I <- as.integer(lambda); f <- ppois(I,lambda); passos <- 1
   if (u<f){
     i <- I; p <- dpois(i,lambda); f <- f-p
     while (u<f){
       i <- i-1; p <- dpois(i,lambda); f <- f-p
+      passos <- passos+1
     }
-    x <- i
-    return(x)
+    return(passos)
   }
   else{
     i <- I+1; p <- dpois(i,lambda); f <- f+p
     while (u>=f){
       i <- i+1; p <- p*lambda/i; f <- f+p
+      passos <- passos+1
     }
-    x <- i+1
-    return(x)
+    return(passos)
   }
 }
-poisson(1000)
+x <- 0
+for (i in 1:1000){
+  x[i] <- poisson(1000)[1]
+}
+mean(x)
