1c3583dd86ca0c167fb9dcc1591c3867076b98d8
Daniel-EST <dd_santos@id.uff.br>
2018-09-18 09:03:00 -0300

    Terminei os exercícios feitos em sala sobre método da composição dia 18/09/18

--- Aulas Praticas/Atividades/Daniel/Sala de Aula/Metodo_da_composicao.R
+++ Aulas Praticas/Atividades/Daniel/Sala de Aula/Metodo_da_composicao.R
@@ -1,9 +1,5 @@
 # Exercício aula 18/09/18
 # Gere Y v.a.
-
-# gerando unif.disc~{1,2,3}
-as.integer(3*runif(1))+1
-
 qa <- function(x){
   t <- as.integer(3*runif(1)) + 1
   if(t == 1) return(runif(1)**(1))
@@ -18,4 +14,36 @@ qb <- function(x){
 }
 
 # Exercício 12 (Cap 5)
+# Considere que x_i ~ Exp(1)
+q12 <- function(lambda,n){
+  x <- NULL
+  for(i in 1:n){
+    x[i] <- -log(1-runif(1))/lambda
+  }
+  return(max(x))
+}
+q12(1, 100)
 
+# Exercício empresa
+
+bern <- function(p){
+  u <- runif(1)
+  if(u >= 1-p) return(1)
+  return(0)
+}
+
+bin <- function(n, p){
+  x <- NULL
+  for(i in 1:n){
+    x[i] <- bern(p)
+  }
+  return(x)
+}
+
+exp <- function(lambda) return(-log(runif(1))/lambda)
+
+custo <- function(n, p, lambda){
+  return(sum(bin(n, p)) * exp(lambda))
+}
+
+custo(1000, 0.05, 1/800)
