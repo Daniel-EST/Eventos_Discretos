ae0db79ca98bde3027db751293c7c17d24406111
Gabriel Mizuno <40043614+GMizuno@users.noreply.github.com>
2018-09-29 21:51:57 -0300

    Finalmente saiu a questao 16 do cap 5

--- Aulas Praticas/Exercicios do livro/Capitulo 5/Exercicio 16 cap 5.R
+++ Aulas Praticas/Exercicios do livro/Capitulo 5/Exercicio 16 cap 5.R
@@ -1,6 +1,6 @@
 #Usando o metodo da rejeição temos que o ponto q maximiza f(x)/g(x) onde g(x)=lambda*exp(-lambda*x)
 #x=2/(1-lambda). Pela teoria sabemos que E[X]=c, queremos minimar E[X], ou seja queremos achar 
-#valor de c(lambda)=2*exp(-2)/((1-lambda)**2*lambda) ==> lambda = 1/3
+#valor de c(lambda) min tal que c(lambda)=2*exp(-2)/(((1-lambda)^2)*lambda) ==> lambda = 1/3
 
 #Gerando Exponencial
 expo <- function(lambda){
@@ -14,11 +14,11 @@ expo <- function(lambda){
 f <- function(lambda){
   y <- expo(lambda)
   u1 <- runif(1)
-  t <- y^2*exp(-y*(1-y))*(1-lambda)^2/(4*exp(-2))
+  t <- y^2*exp(-y*(1-lambda))*(1-lambda)^2/(4*exp(-2))
   while (u1>=t){
     u1 <- runif(1)
     y <- expo(lambda)
-    t <- y^2*exp(-y*(1-y))*(1-lambda)^2/(4*exp(-2))
+    t <- y^2*exp(-y+lambda*y+2)*(1-lambda)^2/4
   }
   x <- y
   return(x)
@@ -27,7 +27,7 @@ f <- function(lambda){
 #Testando 
 
 b <- 0
-for (i in 1:3000){
+for (i in 1:50000){
   b[i] <- f(1/3)
 }
 mean(b)
