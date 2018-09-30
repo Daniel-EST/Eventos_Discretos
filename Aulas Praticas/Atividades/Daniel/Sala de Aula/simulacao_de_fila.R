7a0e4c7e346530bc5ce02f127c4d9756f618cbec
Daniel-EST <dd_santos@id.uff.br>
2018-09-30 15:57:19 -0300

    bug fix, agora o código para simulação de fila funciona corretamente.

--- Aulas Praticas/Atividades/Daniel/Sala de Aula/simulacao_de_fila.R
+++ Aulas Praticas/Atividades/Daniel/Sala de Aula/simulacao_de_fila.R
@@ -1,3 +1,4 @@
+# Fiz um algorítimo recursivo para simular a fila
 fila <- function(Time, l1, l2, t = 0, tc = rexp(1, l1), ts = Inf, n = 0, Nc = 0, Ns = 0, C = NULL, S = NULL, Tp = 0){
   if(min(tc,ts) > Time & n == 0) return(list(Time = Time, Chegadas = C, Saidas = S, Ultimo.tempo = max(0, t-Time)))
   if(min(tc,ts) > Time & n > 0){
@@ -11,8 +12,11 @@ fila <- function(Time, l1, l2, t = 0, tc = rexp(1, l1), ts = Inf, n = 0, Nc = 0,
   if(ts < tc & ts < Time){
     t <- ts ; n <- n - 1
     Ns <- Ns + 1
-    if(n == 0) ts <- Inf
-    ts <- t + rexp(1, l2)
+    if(n == 0){
+      ts <- Inf
+    } else {
+      ts <- t + rexp(1, l2)
+    }
     S[Ns] <- t
     return(fila(Time, l1, l2, t, tc, ts, n, Nc, Ns, C, S, Tp))
   }
@@ -27,4 +31,7 @@ fila <- function(Time, l1, l2, t = 0, tc = rexp(1, l1), ts = Inf, n = 0, Nc = 0,
     return(fila(Time, l1, l2, t, tc, ts, n, Nc, Ns, C, S, Tp))
   }
 } 
-fila(10,1,2)
+
+# Testando
+fila(10,1,2) # Rodei a função algumas vezes e creio que tudo esta correndo bem,
+# não temos chegadas após as 10, nem saídas anteriores as chegadas. 
