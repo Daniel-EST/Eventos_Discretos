394717e7dd3fd9b37d3a24619531dd97b9ba2e94
Daniel-EST <dd_santos@id.uff.br>
2018-10-01 16:14:57 -0300

    Deixei o código mais organizado e adicionei uma solução pelo método da rejeição.

--- Aulas Praticas/Exercicios do livro/Capitulo 4/Exercico 3 cap 4.R
+++ Aulas Praticas/Exercicios do livro/Capitulo 4/Exercico 3 cap 4.R
@@ -1,32 +1,35 @@
 #Exercicio 3 capitulo 4
-
 p <- c(.3,.2,.35,.15)
-
-f <- function( ){
-  x <- 0
+f <- function(){
   u <- runif(1)
-  if (u<.3){
-    x <- 1
-    return(x)
-  }
-  if(u<.5){
-    x <- 2
-    return(x)
-  }
-  if(u<.85){
-    x <- 3
-    return(x)
-  }
-  if (u<1){
-    x <- 4
-    return(x)
-  }
+  if(u < 0.3) return(1)
+  if(u < 0.5) return(2)
+  if(u < 0.85) return(3)
+  return(4)
 }
 
-#testando o algoritmo
+# Pelo método da rejeição
+g <- function(){
+  prob <- c(.3, .2, .35, .15)
+  y <- as.integer(4*runif(1)) + 1
+  while(runif(1) > prob[y]/.35){
+    prob <- c(.3, .2, .35, .15)
+    y <- as.integer(4*runif(1)) + 1
+  }
+  return(y)
+}
 
+# testando o algoritmo
+# Método da transformação inversa
 e <- 0 
 for (i in 1:100000){
   e[i] <- f( )
 }
 mean(e)
+
+# Método da rejeição.
+h <- 0 
+for (i in 1:100000){
+  h[i] <- g()
+}
+mean(h)
